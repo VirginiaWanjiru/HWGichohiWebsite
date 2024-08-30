8ddbf4c25968097e6bcd224be75293c39365ec1d
Virginia Wanjiru <virginiashee55@gmail.com>
2024-08-30 12:28:53 +0300

    allowfullscreen

--- components/ui/contact-map.tsx
+++ components/ui/contact-map.tsx
@@ -27,7 +27,7 @@ export default function ContactMap(){
                     width='100%'
                     height='100%'
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.7485957130043!2d36.84073871059158!3d-1.3268039986550237!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f1174a3c1e8db%3A0x3704e328dcdce4fd!2sH.%20W.%20Gichohi%20%26%20Co.!5e0!3m2!1sen!2ske!4v1724526847762!5m2!1sen!2ske"
-                    style={{border :0}} allowFullScreen="" loading="lazy"
+                    style={{border :0}} allowFullScreen loading="lazy"
                     referrerPolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
