12fbc522e0cc3e4c7981f48caff37118a941a954
Virginia Wanjiru <virginiashee55@gmail.com>
2024-10-03 13:44:17 +0300

    HW Gichohi CPA (K)

--- components/BannerSlider/banner.tsx
+++ components/BannerSlider/banner.tsx
@@ -6,6 +6,7 @@ import { useRouter } from 'next/navigation';
 interface Slide {
   image: string;
   text: string;
+  name:string,
   buttonText: string;
   buttonLink: string;
 }
@@ -18,24 +19,28 @@ const Banner: React.FC = () => {
     {
       image: "/images/image1.jpg",
       text: "Delivering Results in Assurance, Advisory and Compliance",
+      name:"H.W GICHOHI AND CO CPA (K)",
       buttonText: "Contact Us",
       buttonLink: "/#contact-us",
     },
     {
       image: "/images/nairobi5.jpg",
       text: "Tailored Financial Solutions for Your Business",
+      name:"H.W GICHOHI AND CO CPA (K)",
       buttonText: "Learn More",
       buttonLink: "/#learn-more",
     },
     {
       image: "/images/nairobi3.jpg",
       text: "Partnering with You for Sustainable Growth",
+      name:"H.W GICHOHI AND CO CPA (K)",
       buttonText: "Our Services",
       buttonLink: "/#services",
     },
     {
       image: "/images/nairobi.jpg",
       text: "Trusted Advisors, Committed to Your Success",
+      name:"H.W GICHOHI AND CO CPA (K)",
       buttonText: "Get Started",
       buttonLink: "/#get-started",
     },
@@ -84,7 +89,9 @@ const Banner: React.FC = () => {
                 <div className="flex flex-col w-full h-full px-8 md:px-8 gap-8 md:max-w-4xl md:gap-8 text-[#FFFFFF] mt-60">
                   <h1 className="md:text-start mt-24 md:text-6xl font-serif">
                     {slide.text}
+                    
                   </h1>
+                  <p>{slide.name}</p>
                   <button
                     onClick={() => router.push(slide.buttonLink)}
                     className="bg-[#C00000] py-4 font-extrabold uppercase md:w-1/3 max-w-[20rem] rounded-2xl md:text-2l hover:bg-sky-700 mt-8"
