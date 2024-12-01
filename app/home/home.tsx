05b7f4e3c94c5158e08b9ec58a6504207ec21d0f
Virginia Wanjiru <virginiashee55@gmail.com>
2024-12-01 21:12:10 +0300

    H.W Gichohi

--- app/home/home.tsx
+++ app/home/home.tsx
@@ -62,6 +62,8 @@ export default function Home() {
                 </span>
                 <br />
                 <br />
+                <br />
+                
 
                 {/* Button */}
                 <a href="/contact">
