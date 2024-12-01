9dacff251ea66034671d3ee597fae3471678ffff
Virginia Wanjiru <virginiashee55@gmail.com>
2024-12-01 17:26:22 +0300

    Gichohi

--- app/profile/Gichohi/page.tsx
+++ app/profile/Gichohi/page.tsx
@@ -7,8 +7,57 @@ import { motion } from "framer-motion";
 import { FaDownload, FaArrowLeft } from "react-icons/fa"; // Import the back icon
 import ExecutiveImage from "@/public/images/bg3.jpg";
 
+const profile = [
+  {
+    id: 1,
+    name: "Background and Experience ",
+
+    description:
+      "Hezekiah Wang’ombe Gichohi is currently the Consulting Partner at H.W.Gichohi & Company CPA(K). With over 58 years of cumulative experience in auditing, taxation, and management consultancy, Hezekiah has built a remarkable career specializing in organizational development, strategic financial management, and the restructuring of businesses. Mr. Hezekiah has extensive expertise in auditing for diverse sectors, including insurance, banking, NGOs, manufacturing, and quasi-government institutions. He has played a pivotal role in turning around financially distressed organizations, including managing over 15 companies under receivership and successfully restoring them to profitability. Mr. Hezekiah has contributed significantly to the accounting profession. He initiated the acquisition of ICPAK's CPA Centre. He also initiated the establishment of the Mhasibu Sacco Society in 1986, which has grown to over 23,000 members globally. Beyond his professional achievements, Hezekiah has a passion for community service. He founded a water company that serves over 200 families, providing annual dividends of up to 30% for shareholders. As a mentor and leader, he has trained and guided countless professionals in their careers while serving as a patron for schools and health centers in his community.",
+ 
+ options:[]
+    },
+
+  {
+    id: 2,
+    name: "Educational & Professional Qualifications",
+    options: ["1968 - 1972: Kenya Polytechnic – ACCA"],
+  },
+
+  {
+    id: 3,
+    name: " Professional Membership",
+    options: [
+      "Fellow of the Institute of Certified Public Accountants of Kenya (ICPAK)",
+      "Fellow of the Association of Chatered Accountants, London (ACCA)",
+      "Fellow of the Kenya Institute of Management",
+      "Professional Trainers Association",
+    ],
+    
+  },
+  {
+    id: 4,
+    name: "Career Summary",
+    options: [
+      "1966 – 1974: Audit Assistant, Delloite & Touche, Certified Public Accountants",
+      "1974 – 1976: Senior Accountant, Delloite & Touche, Certified Public Accountants",
+      " 1976 – 1979: Audit Senior, Delloite & Touche, Certified Public  Accountants",
+      "1979 – 1985: Managing Partner, Wanyeki Gichohi & Company, Certified Public Accountants",
+      "1986 – 2011: Managing Partner, H.W. Gichohi & Co. CPA(K)",
+      "2011 - Present : Consulting Partner, H.W. Gichohi & Co. CPA(K)",
+    ],
+  },
+];
+
 export default function Gichohi() {
   const [hovered, setHovered] = useState(false);
+
+  const [expandedGichohi, setExpandedGichohi] = useState<number | null>(null);
+
+  const toggleExpand = (id: number) => {
+    // If the same section is clicked, collapse it. Otherwise, expand the new section.
+    setExpandedGichohi((prevState) => (prevState === id ? null : id));
+  };
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-white">
@@ -93,83 +142,49 @@ export default function Gichohi() {
         </motion.div>
       </div>
 
-      <div className=" p-20 ">
-        <h1 className="text-4xl text-red-900 mb-10 ">
-          Background & Expertise{" "}
-        </h1>
-        <p className="text-xl text-black text-balance">
-          Hezekiah Wang’ombe Gichohi is currently the Consulting Partner at H.W.
-          Gichohi & Company CPA(K). With over 58 years of cumulative experience
-          in auditing, taxation, and management consultancy, Hezekiah has built
-          a remarkable career specializing in organizational development,
-          strategic financial management, and the restructuring of businesses.
-          Mr. Hezekiah has extensive expertise in auditing for diverse sectors,
-          including insurance, banking, NGOs, manufacturing, and
-          quasi-government institutions. He has played a pivotal role in turning
-          around financially distressed organizations, including managing over
-          15 companies under receivership and successfully restoring them to
-          profitability. Mr. Hezekiah has contributed significantly to the
-          accounting profession. He initiated the acquisition of ICPAK's CPA
-          Centre. He also initiated the establishment of the Mhasibu Sacco
-          Society in 1986, which has grown to over 23,000 members globally.
-          Beyond his professional achievements, Hezekiah has a passion for
-          community service. He founded a water company that serves over 200
-          families, providing annual dividends of up to 30% for shareholders. As
-          a mentor and leader, he has trained and guided countless professionals
-          in their careers while serving as a patron for schools and health
-          centers in his community.
-        </p>
+      <div className="mt-10 ml-20 ">
+        {profile.map((profiles) => (
+          <div
+            key={profiles.id}
+            id={profiles.name.toLowerCase().replace(/[^a-z]+/g, "-")} // Converts the name to a URL-friendly id
+            onClick={() => toggleExpand(profiles.id)}
+            className=" mb-6 p-6 bg-neutral-100 rounded-lg shadow-lg ml-20 mr-20 hover:bg-gradient-to-r from-gray-300 via red-200 to-red-800 "
+          >
+            <div className="flex justify-between items-center">
+              {/* Section Title */}
+              <h2 className="text-lg font-bold">{profiles.name}</h2>
 
-        <h1 className="text-4xl text-red-900 mb-10 mt-10 ">
-          Educational & Professional Qualifications{" "}
-        </h1>
-        <p className="text-xl text-black text-base">
-          <ul>
-            <li>1968 - 1972: Kenya Polytechnic – ACCA </li>
-          </ul>
-        </p>
+              {/* Expand Icon */}
+              <button
+                onClick={() => toggleExpand(profiles.id)}
+                className="text-xl text-gray-500 hover:text-gray-700"
+              >
+                {expandedGichohi === profiles.id ? "-" : "+"}
+              </button>
+            </div>
 
-        <h1 className="text-4xl text-red-900 mb-10 mt-10 ">
-          Professional Membership
-        </h1>
-        <p className="text-xl text-black text-base">
-          <ul>
-            <li>
-              Fellow of the Institute of Certified Public Accountants of Kenya
-              (ICPAK){" "}
-            </li>
-            <li>
-              Fellow of the Association of Chatered Accountants, London (ACCA){" "}
-            </li>
-            <li>Fellow of the Kenya Institute of Management </li>
-            <li>Professional Trainers Association</li>
-          </ul>
-        </p>
+            {/* Service Description and Options */}
+            <div
+              className={`transition-all duration-500 ease-in-out overflow-hidden ${
+                expandedGichohi === profiles.id ? "max-h-[500px]" : "max-h-0"
+              }`}
+            >
+              {expandedGichohi === profiles.id && (
+                <div className="mt-4">
+                  <p className="mt-4 text-lg text-gray-700">
+                    {profiles.description}
+                  </p>
 
-        <h1 className="text-4xl text-red-900 mb-10 mt-10 ">Career Summary</h1>
-
-        <p className="text-xl text-black text-base">
-          <ul>
-            <li>
-              1966 – 1974: Audit Assistant, Delloite & Touche, Certified Public
-              Accountants
-            </li>
-            <li>
-              1974 – 1976: Senior Accountant, Delloite & Touche, Certified
-              Public Accountants
-            </li>
-            <li>
-              1976 – 1979: Audit Senior, Delloite & Touche, Certified Public
-              Accountants
-            </li>
-            <li>
-              1979 – 1985: Managing Partner, Wanyeki Gichohi & Company,
-              Certified Public Accountants
-            </li>
-            <li>1986 – 2011: Managing Partner, H.W. Gichohi & Co. CPA(K)</li>
-            <li>2011 to date: Consulting Partner, H.W. Gichohi & Co. CPA(K)</li>
-          </ul>
-        </p>
+                  <p className="list-disc ml-4 text-lg">
+                    {profiles.options.map((option, index) => (
+                      <li key={index}>{option}</li>
+                    ))}
+                  </p>
+                </div>
+              )}
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
