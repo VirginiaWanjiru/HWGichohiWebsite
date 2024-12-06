9c9acfef78ca5db9ad58d176f59735480aa444b0
Virginia Wanjiru <virginiashee55@gmail.com>
2024-12-06 09:25:27 +0300

    banner

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
 import Banner from "@/public/images/nairobi.jpg";
+import Nairobi from "@/public/images/nairobi5.webp";
 
 import { useState } from "react";
 
@@ -206,8 +207,48 @@ export default function Services() {
                   </div>
                 )}
               </div>
+
+              
             </div>
           ))}
+        </div>
+        {/* Full-width Image with Text and Button */}
+        <div className="relative  mt-20 ">
+          
+          
+          <div className="relative w-full h-[50vh]">
+            <Image
+              src={Nairobi}
+              alt="Full-width Banner"
+              layout="fill"
+              objectFit="cover"
+              className="opacity-1"
+            />
+          </div>
+          {/* Background Blur Overlay */}
+          <div className="absolute inset-0  bg-opacity-2 backdrop-blur-lg z-0 "></div>
+          <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
+            <div className="text-center text-white max-w-3xl px-6 py-4">
+              
+
+              {/* Additional Text */}
+              <span className="font-bold text-xl text-white">
+                Contact us today to schedule a personalized one-on-one
+                consultation.
+              </span>
+              <br />
+              <br />
+              <br />
+              
+
+              {/* Button */}
+              <a href="/contact">
+                <button className="px-10 py-4 bg-red-700 text-white font-bold  hover:bg-red-900 text-lg rounded ">
+                CONTACT US
+                </button>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </>
